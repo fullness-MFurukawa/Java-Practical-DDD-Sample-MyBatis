c6f8f3e4768271cef4df32632a6d162566b738aa
Masatoshi Furukawa <furukawa@fullness.co.jp>
2026-07-27 09:08:35 +0900

    first commit

---
+++ src/main/java/jp/co/fullness/ddd/sample/PracticalSampleMybatisApplication.java
@@ -0,0 +1,13 @@
+package jp.co.fullness.ddd.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalSampleMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalSampleMybatisApplication.class, args);
+	}
+
+}
